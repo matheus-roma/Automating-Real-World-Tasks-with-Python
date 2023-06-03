cd6850c3a19454def20410119e7d857940e2c0bd
Matheus Roma <matroma@pop-os.localdomain>
2023-06-02 21:09:43 -0300

    requests and test practice

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import json
+import json, requests
+
+url = 'https://www.google.com'
+response = requests.get(url)
+
+# if not response.ok:
+#     raise Exception("GET failed with status code {}".format(response.status_code))
+
+response.raise_for_status()
+
 
 with open("people.json", "r") as people_json:
     people = json.load(people_json)
 
-#print(people)
+print(response.status_code)
