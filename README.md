c8efa8db24606bafdb7e3ea3832bb2cbd1c089e6
Matheus Roma <matroma@pop-os.localdomain>
2023-06-03 12:58:33 -0300

    finishing the lab2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # interacting-with-web-services
 Week 2 of Automating Real-World Tasks with Python
+
+The premisse of this task it's that you're working at a company that sells second-hand cars. Your company constantly collects feedback in the form of customer reviews. Your manager asks you to take those reviews (saved as .txt files) and display them on your company's website. To do this, you'll need to write a script to convert those .txt files and process them into Python dictionaries, then upload the data onto your company's website (currently using Django).
+
+For this lab, a Django web server corpweb was configured under `/projects/corpweb` directory. One could check it out by visiting the external IP address of the corpweb VM. Unfortunatelly this was deleted after the test.
--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import json, requests, os
 
-resourcesDir = "resources"
+feedbackDir = "data/feedback"
+corpweb_external_IP = "35.222.178.179"
 
-data = {}
-for filename in os.listdir(resourcesDir):
-    file_path = os.path.join(resourcesDir, filename)
+data = []
+for filename in os.listdir(feedbackDir):
+    file_path = os.path.join(feedbackDir, filename)
     if file_path.endswith(".txt"):
         with open(file_path, "r") as file:
-            data[filename] = {"title" : file.readline().strip("\n"),
+            data.append({"title" : file.readline().strip("\n"),
                               "name" : file.readline().strip("\n"),
                               "date" : file.readline().strip("\n"),
-                              "feedback" : file.readline().strip("\n")}
+                              "feedback" : file.readline().strip("\n")})
 
-#with open("feedbacks.json", "w") as feedbacks_json:
-#    json.dump(data, feedbacks_json, indent=2)
+# with open("feedbacks.json", "w") as feedbacks_json:
+#     json.dump(data, feedbacks_json, indent=2)
 
-#print(data)
-
-
-            
+for item in data:
+    web_feedback = requests.post(f"http://{corpweb_external_IP}/feedback/", data=item)
+    web_feedback.raise_for_status()
