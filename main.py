ffaa26c9b54fb947221755a8cb7e2fe6763f0f54
Matheus Roma <matroma@pop-os.localdomain>
2023-06-03 12:02:26 -0300

    converting txt to json

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ for filename in os.listdir(resourcesDir):
     file_path = os.path.join(resourcesDir, filename)
     if file_path.endswith(".txt"):
         with open(file_path, "r") as file:
-            data[filename] = file.read()
+            data[filename] = {"title" : file.readline().strip("\n"),
+                              "name" : file.readline().strip("\n"),
+                              "date" : file.readline().strip("\n"),
+                              "feedback" : file.readline().strip("\n")}
 
-print(data)
+#with open("feedbacks.json", "w") as feedbacks_json:
+#    json.dump(data, feedbacks_json, indent=2)
+
+#print(data)
 
 
             
