c142d11b33639c235343f580d4b87225d3223d24
Matheus Roma <matroma@pop-os.localdomain>
2023-06-06 19:06:34 -0300

    using emailmessage

--- automatically-generate-a-pdf-and-send-it-by-email/main.py
+++ automatically-generate-a-pdf-and-send-it-by-email/main.py
@@ -5,4 +5,21 @@ message = EmailMessage()
 sender = "me@example.com"
 recipient = "you@example.com"
 
+message['From'] = sender
+message['To'] = recipient
+message['Subject'] = 'Greetings from {} to {}!'.format(sender, recipient)
+
+#From, To, and Subject are examples of email header fields
+
+#They’re key-value pairs of labels and instructions used by 
+# email clients and servers to route and display the email
+
+#They’re separate from the email's message body, which is 
+# the main content of the message.
+
+body = """Hey there!"""
+
+
+
+
 print(message)
