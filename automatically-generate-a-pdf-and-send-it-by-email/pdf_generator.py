4e32fe3fedafdef82c77cc045e38292829d5bb15
Matheus Roma <matroma@pop-os.localdomain>
2023-06-07 16:36:12 -0300

    finish pdf_generator

--- automatically-generate-a-pdf-and-send-it-by-email/pdf_generator.py
+++ automatically-generate-a-pdf-and-send-it-by-email/pdf_generator.py
@@ -1,6 +1,8 @@
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
 import os
 
 fruit = {
@@ -16,6 +18,27 @@ fruit = {
 cwd = os.getcwd()
 report = SimpleDocTemplate(f"{cwd}/resources/report.pdf")
 styles = getSampleStyleSheet()
-
 report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
-report.build([report_title])
+
+table_data = []
+for k, v in fruit.items():
+  table_data.append([k, v])
+
+#print(table_data)
+
+table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+
+report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+
+report_pie = Pie(width=10, height=10)
+report_pie.data = []
+report_pie.labels = []
+
+for fruit_name in sorted(fruit):
+  report_pie.data.append(fruit[fruit_name])
+  report_pie.labels.append(fruit_name)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+report.build([report_title, report_table, report_chart])
